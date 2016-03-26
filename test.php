0fcb88c7e12f4469b43c05e6e62bf3fcfdfaaf03
ityike <yuanfeng634@gmail.com>
2016-03-26 16:34:02 +0800

    add a test

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ityike
+ * Date: 16/3/26
+ * Time: 14:37
+ */
+
+/*$str1 = 'hello bift!';
+$str2 = 'hello beifu';
+$bift = array('str1','str2');
+$biftArry = compact($bift);
+var_dump($biftArry);*/
+
